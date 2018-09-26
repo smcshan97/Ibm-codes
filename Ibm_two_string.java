b7b55178a62b4f4a1601eaf910aca73b6bbf950b
smcshan97 <43617447+smcshan97@users.noreply.github.com>
2018-09-26 13:47:40 -0400

    Update Ibm_two_string.java

--- Ibm_two_string.java
+++ Ibm_two_string.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
 public class Ibm_two_string {
-	 static Set<Character> a;
-	    static Set<Character> b;
+	static Set<Character> x;
+	static Set<Character> y;
 
-	    public static void main(String[] args) {
-	        Scanner scan = new Scanner(System.in);
-	        int n = scan.nextInt();
-	        for(int i = 0; i < n; i++) {
-	            a = new HashSet<Character>();
-	            b = new HashSet<Character>();
-	            for(char c : scan.next().toCharArray()) {
-	                a.add(c);
-	            }
-	            for(char c : scan.next().toCharArray()) {
-	                b.add(c);
-	            }
-	            
-	            // store the set intersection in set 'a'
-	            a.retainAll(b);
-	            
-	            System.out.println( (a.isEmpty()) ? "NO" : "YES" );
-	        }
-	        scan.close();
-	    }
+	public static void main(String[] args) 
+	{
+		// I'm assuming the problem wants a scanner but it is not specific, I can resubmit if needed
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		
+		for(int i = 0; i < n; i++) {
+			x = new HashSet<Character>();
+			y = new HashSet<Character>();
+			for(char s : scan.next().toCharArray())
+			{
+				x.add(s);
+			}
+			for(char s : scan.next().toCharArray()) 
+			{
+				y.add(s);
+			}
+
+			x.retainAll(y);
+
+			System.out.println( (x.isEmpty()) ? "NO" : "YES" );
+		}
+		scan.close();
+	}
 }
