1244962d95992c920a267e10d391a79a12de6370
smcshan97 <43617447+smcshan97@users.noreply.github.com>
2018-09-26 13:48:08 -0400

    Update Ibm_left_rotation.java

--- Ibm_left_rotation.java
+++ Ibm_left_rotation.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 public class Ibm_left_rotation {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		// I'm assuming the problem wants a scanner but it is not specific, I can resubmit if needed
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int d = scan.nextInt();
-        int[] a= new int[n];
-    for(int i=0;i<n;a[i]=scan.nextInt(),i++);
-   	 d=d%n;
-    for(int i=d;i<n;i++)
-        System.out.print(a[i]+" ");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int[] a= new int[x];
+        
+   for(int i=0;i<x;a[i]=scan.nextInt(),i++);
+   	     y=y%x;
     
-    for(int i=0;i<d;i++)
+   for(int i=y;i<x;i++)
+         System.out.print(a[i]+" ");
+    
+   for(int i=0;i<y;i++)
          System.out.print(a[i]+" ");
     }
 }
